3f1fdd8ec243a40952e288b2f1e57e7a9374a433
alexandr nikandrov <dafen@ukr.net>
2021-05-21 07:24:15 +0300

    create a simple bot

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // ctc-bot
 
+const TelegramBot = require('node-telegram-bot-api');
+
+const TOKEN = '1818383467:AAFTPSct-_jkXgCIz3eRtdPpTFVVXX0jyrY';
+
+const bot = new TelegramBot (TOKEN, {
+    polling: true
+})
+
+bot.on('message', (msg) => {
+    console.log(msg)
+    bot.sendMessage(msg.chat.id, 'hello, ' + msg.from.first_name)
+})
 
 
