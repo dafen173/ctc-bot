dc749205e6f63a3762f0257fdee80d201a6e05fb
Alex <dafen@ukr.net>
2021-05-26 10:38:27 +0300

    lesson 15

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const TelegramBot = require('node-telegram-bot-api');
 
-const TOKEN = '1818383467:AAFTPSct-_jkXgCIz3eRtdPpTFVVXX0jyrY';
+const TOKEN = '1818383467:AAE0o9-CnjnUF8K2zV2ITz2mrSSJC4qDLsI';
 
 console.log('бот был запущен...')
 
@@ -18,4 +18,27 @@ const bot = new TelegramBot (TOKEN, {
 })
 
 const debug = require( './helpers' );
+
+
+bot.on('inline_query', query => {
+
+    const results = []
+
+    for (let i = 0; i < 5; i++) {
+        results.push({
+            type: 'article',
+            id: i.toString(),
+            title: 'Title ' + i,
+            input_message_content: {
+                message_text: `Article #${i+1}`
+            }
+        })        
+    }
+
+    bot.answerInlineQuery(query.id, results, {
+        cashe_time: 0
+    })
+
+})
+
 
