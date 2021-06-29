8725b1a87e1f084a37d2954fc7f298dc9a4aebc6
Alex <dafen@ukr.net>
2021-06-29 10:10:24 +0300

    lesson 45.2

--- src/index.js
+++ src/index.js
@@ -298,12 +298,24 @@ function showFavouriteFilms(chatId, telegramId) {
     .then(user => {
 
         if (user) {
+            Film.find({uuid: {'$in': user.films}}).then(films => {
+                let html
 
+                if (films.length) {
+                    html = films.map((f, i) => {
+                        return `<b>${i + 1}</b> ${f.name} - <b>${f.rate}</b> /f${f.uuid}`
+                    }).join('\n')
+                } else {
+                    html = 'Вы пока ничего не добавили'
+                }
+
+                sendHTML(chatId, html, 'home')
+            }).catch(e => console.log(e))
         } else {
             sendHTML (chatId, 'Вы пока ничего не добавили', 'home')
         }
 
-    })
+    }).catch(e => console.log(e))
 }
 
 
